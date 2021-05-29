5d823590396fb65f60219e9ce705b8203a4bd206
Nathan Craddock <nzcraddock@gmail.com>
2021-05-29 14:09:10 -0600

    Add basic config support (#3)
    Adds support for default settings and user preferences.
    The only value is "ignore" which stores a list of default ignore
    directories.
    The preferences are unused at this point.
    
    Closes #3

--- lua/nvim-find/init.lua
+++ lua/nvim-find/init.lua
@@ -1,7 +1,12 @@
 local find_file = require("nvim-find.defaults.files")
+local config = require("nvim-find.config")
 
 local nvim_find = {}
 
+-- User configuration
+nvim_find.setup = config.setup
+
+-- Default file finder
 nvim_find.files = find_file.open
 
 return nvim_find
