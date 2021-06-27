87d117755626857bcd1784cfb8fac4491a7634eb
Nathan Craddock <nzcraddock@gmail.com>
2021-06-26 22:54:42 -0600

    Add fallback filter to filename matching
    If no results are found for filename matching, then the filter will
    run a second time with a more lenient search algorithm that matches
    against the entire path.

--- lua/nvim-find/filters/filename.lua
+++ lua/nvim-find/filters/filename.lua
@@ -15,9 +15,7 @@ local function has_delimiters(value)
 end
 
 -- Creates a filter that uses the given query
-local function filename_filter(query, ignore_case, ignore_delimiters)
-  query = utils.str.trim(query)
-
+local function filename_filter(query, ignore_case, ignore_delimiters, full_path)
   -- Should we ignore case?
   if ignore_case == nil then
     ignore_case = not has_upper(query)
@@ -38,7 +36,12 @@ local function filename_filter(query, ignore_case, ignore_delimiters)
     if ignore_case then value = value:lower() end
     if ignore_delimiters then value = value:gsub(DELIMITERS, "") end
 
-    local filename = utils.path.basename(value)
+    local filename
+    if full_path then
+      filename = value
+    else
+      filename = utils.path.basename(value)
+    end
     if not string.find(filename, tokens[1], 0, true) then
       return false
     end
@@ -60,11 +63,28 @@ end
 
 function file.run(source, ignore_case, ignore_delimiters)
   return function(finder)
+    local query = utils.str.trim(finder.query)
+
+    local had_results = false
     for results in async.iterate(source, finder) do
       if type(results) == "table" then
-        coroutine.yield(vim.tbl_filter(filename_filter(finder.query, ignore_case, ignore_delimiters), results))
+        local filtered = vim.tbl_filter(filename_filter(query, ignore_case, ignore_delimiters), results)
+        if #filtered > 0 then had_results = true end
+
+        coroutine.yield(filtered)
       else
         coroutine.yield(results)
+      end
+    end
+
+    -- No results, fall back on full path match
+    if not had_results then
+      for results in async.iterate(source, finder) do
+        if type(results) == "table" then
+          coroutine.yield(vim.tbl_filter(filename_filter(query, ignore_case, ignore_delimiters, true), results))
+        else
+          coroutine.yield(results)
+        end
       end
     end
   end
